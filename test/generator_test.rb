197a5e26c3297eca4601dc658ccfd3cd01ca24ea
Cameron Bothner <cameronbothner@gmail.com>
2019-03-18 22:35:01 -0400

    Fix generator test for new location

--- test/generator_test.rb
+++ test/generator_test.rb
@@ -3,14 +3,14 @@ require "generators/markerb/mailer/mailer_generator"
 
 class GeneratorTest < Rails::Generators::TestCase
   tests Markerb::Generators::MailerGenerator
-  destination File.expand_path("../tmp", __FILE__)
+  destination File.expand_path("tmp", __dir__)
   setup :prepare_destination
 
   test "assert all views are properly created with given name" do
-    run_generator %w(notifier foo bar baz)
+    run_generator %w[notifier foo bar baz]
 
-    assert_file "app/views/notifier/foo.markerb"
-    assert_file "app/views/notifier/bar.markerb"
-    assert_file "app/views/notifier/baz.markerb"
+    assert_file "app/views/notifier_mailer/foo.markerb"
+    assert_file "app/views/notifier_mailer/bar.markerb"
+    assert_file "app/views/notifier_mailer/baz.markerb"
   end
 end
